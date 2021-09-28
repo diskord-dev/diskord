61456332b3261fff1246d0de403444d07e8bc1c3
Izhar Ahmad <54180221+nerdguyahmad@users.noreply.github.com>
2021-09-28 18:38:15 +0500

    Create option_names.py

---
+++ examples/application_commands/option_names.py
@@ -0,0 +1,17 @@
+import diskord
+from diskord import commands
+
+bot = commands.Bot(command_prefix='!')
+
+# you can set "arg" keyword argument to the name of argument that represents the option in the command function
+# and then change the option name as desired.
+@bot.slash_command()
+@bot.slash_option('sentence', arg='text', description='The text to say!')
+async def say(ctx, text):
+  await ctx.send(f'{ctx.author.name} said: {text}')
+
+# in above command, the option name in discord will appear "sentence" but in this function, it will
+# be passed to text argument
+  
+  
+bot.run('token')
